ce667e3c1a18853911b45643a31af9fe38b4a8b8
Ben Yomtov <benyomtov@comcast.net>
2023-12-28 04:30:20 -0500

    created logic and functionality for child support calculator

--- income_frontend/src/components/ChildSupport/ChildSupport.jsx
+++ income_frontend/src/components/ChildSupport/ChildSupport.jsx
@@ -1,17 +1,207 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './ChildSupport.css';
 
 const ChildSupport = () => {
-    return (
-        <div className="childsupport">
-        <div className="childsupport__content">
-            <h1 className="childsupport__title">Child Support</h1>
-            <p className="childsupport__description">Calculate Child Support</p>
-            <Link to="/" >Back</Link>
 
+    const [childPayments, setChildPayments] = useState([]);
+    const [numChildren, setNumChildren] = useState('one child');
+    const [isLumpSum, setIsLumpSum] = useState([]);
+    const [annualChildSupport, setAnnualChildSupport] = useState(null);
+
+    const handleInputChange = (index, value) => {
+        const newChildPayments = [...childPayments];
+        newChildPayments[index] = value;
+        setChildPayments(newChildPayments);
+    };
+
+    const handleLumpSumChange = (index, checked) => {
+        const newIsLumpSum = [...isLumpSum];
+        newIsLumpSum[index] = checked;
+        setIsLumpSum(newIsLumpSum);
+    };
+
+    const handleaddChildPayment = () => {
+        setChildPayments([...childPayments, 0]);
+        setIsLumpSum([...isLumpSum, false]);
+    };
+
+    const handleRemoveChildPayment = (index) => {
+        const newChildPayments = [...childPayments];
+        newChildPayments.splice(index, 1);
+        setChildPayments(newChildPayments);
+
+        const newIsLumpSum = [...isLumpSum];
+        newIsLumpSum.splice(index, 1);
+        setIsLumpSum(newIsLumpSum);
+    };
+
+    const calculateAnnualChildSupport = () => {
+
+        const parsedChildPayments = childPayments.map((childPayment) => parseFloat(childPayment));
+
+        if (parsedChildPayments.some(isNaN) 
+        || parsedChildPayments.length === 0
+        || isLumpSum.length !== parsedChildPayments.length) {
+
+            setAnnualChildSupport({
+                annualChildSupport: 'Invalid Input',
+            });
+            return;
+        }
+
+
+        let totalChildPayments = 0;
+
+        // for (let i = 0; i < parsedChildPayments.length; i++) {
+
+        //     const isLumpSumPayment = isLumpSum[i];
+        //     const currentChildPayment = parsedChildPayments[i];
+
+        //     switch (numChildren) {
+        //         case 'one child':
+        //             if (isLumpSumPayment) {
+        //                 let childPaymentNotRounded = currentChildPayment * 0.3;
+        //                 let childPaymentRounded = Math.round(childPaymentNotRounded * 100) / 100;
+        //                 let childPayment = childPaymentRounded.toFixed(2);
+
+        //                 totalChildPayments += childPayment;
+        //             } else {
+        //                 totalChildPayments += currentChildPayment;
+        //             }
+        //             break;
+        //         case 'two children':
+        //             if (isLumpSumPayment) {
+        //                 let childPaymentNotRounded = currentChildPayment * 0.5;
+        //                 let childPaymentRounded = Math.round(childPaymentNotRounded * 100) / 100;
+        //                 let childPayment = childPaymentRounded.toFixed(2);
+
+        //                 totalChildPayments += childPayment;
+        //             } else {
+        //                 totalChildPayments += currentChildPayment;
+        //             }
+        //             break;
+        //         default:
+        //             setAnnualChildSupport(null);
+        //             return;
+        //     }
+        // }
+
+        for (let i = 0; i < parsedChildPayments.length; i++) {
+            const isLumpSumPayment = isLumpSum[i];
+            const currentChildPayment = parsedChildPayments[i];
+          
+            switch (numChildren) {
+              case 'one child':
+                if (isLumpSumPayment) {
+                  let lumpSumChildPaymentNotRounded = currentChildPayment * 0.3;
+                  let lumpSumChildPaymentRounded =
+                    Math.round(lumpSumChildPaymentNotRounded * 100) / 100;
+                  let lumpSumChildPayment = lumpSumChildPaymentRounded.toFixed(2);
+          
+                  totalChildPayments += parseFloat(lumpSumChildPayment);
+                } else {
+                  totalChildPayments += currentChildPayment;
+                }
+                break;
+              case 'two children':
+                if (isLumpSumPayment) {
+                  let lumpSumChildPaymentNotRounded = currentChildPayment * 0.5;
+                  let lumpSumChildPaymentRounded =
+                    Math.round(lumpSumChildPaymentNotRounded * 100) / 100;
+                  let lumpSumChildPayment = lumpSumChildPaymentRounded.toFixed(2);
+          
+                  totalChildPayments += parseFloat(lumpSumChildPayment);
+                } else {
+                  totalChildPayments += currentChildPayment;
+                }
+                break;
+              default:
+                setAnnualChildSupport(null);
+                return;
+            }
+          }
+
+        let annualChildSupportNotRoundedHalf = (totalChildPayments / 6);
+        let annualChildSupportRoundedHalf = Math.round(annualChildSupportNotRoundedHalf * 100) / 100;
+        let annualChildSupportHalf = annualChildSupportRoundedHalf.toFixed(2);
+        let annualChildSupportNotRounded = annualChildSupportHalf * 12;
+        let annualChildSupportRounded = Math.round(annualChildSupportNotRounded * 100) / 100;
+        let annualChildSupport = annualChildSupportRounded.toFixed(2);
+
+
+        setAnnualChildSupport({
+            annualChildSupport,
+        });
+    };
+
+    const handleNumChildrenChange = (event) => {
+        setNumChildren(event.target.value);
+    };
+
+
+    return (
+      <div className="childsupport">
+        <div className="childsupport__content">
+          <h1 className="childsupport__title">Child Support</h1>
+          <p className="childsupport__description">Calculate Child Support</p>
+
+          <label>
+            Number of Children:
+            <select
+                value={numChildren}
+                onChange={handleNumChildrenChange}
+            >
+                <option value="one child">One Child</option>
+                <option value="two children">Two Children</option>
+            </select>
+          </label>
+
+          {childPayments.map((childPayment, index) => (
+            <div key={index} className="childsupport__input-container">
+              <label>
+                Child Support #{index + 1}:
+                <input
+                  type="number"
+                  value={childPayment}
+                  onChange={(event) =>
+                    handleInputChange(index, event.target.value)
+                  }
+                />
+              </label>
+                <label>
+                    Is this a lump sum payment?
+                    <input
+                        type="checkbox"
+                        checked={isLumpSum[index]}
+                        onChange={(event) =>
+                            handleLumpSumChange(index, event.target.checked)
+                        }
+                    />
+                </label>
+            </div>
+          ))}
+            <div className="childsupport__buttons-container">
+                <button className="childsupport__button" onClick={handleaddChildPayment}>Add Child Support</button>
+                <button className="childsupport__button" onClick={handleRemoveChildPayment}>Remove Child Support</button>
+                <button className="childsupport__button" onClick={calculateAnnualChildSupport}>Calculate Annual Child Support</button>
+            </div>
+            {annualChildSupport !== null && (
+                <div className="childsupport__result-container">
+                    <p className="childsupport__result">
+                            Annual Child Support: 
+                            <strong>
+                                {annualChildSupport.annualChildSupport}
+                            </strong>
+                            
+                    </p>
+
+                </div>
+            )}
+
+          <Link to="/">Back</Link>
         </div>
-        </div>
+      </div>
     );
     }
 
