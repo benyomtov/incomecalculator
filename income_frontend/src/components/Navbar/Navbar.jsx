d052cde0d61bd84e5828170c9b59d1cd1e7c561d
Ben Yomtov <benyomtov@comcast.net>
2023-12-31 15:08:33 -0500

    redeploying

--- income_frontend/src/components/Navbar/Navbar.jsx
+++ income_frontend/src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = ({ handleNavbarClick }) => {
     <div className="navbar container-fluid">
       <h1 className="container-fluid justify-content-center">CCAP Income Calculator</h1>
       <div className="navbar__content container-fluid justify-content-start">
-        <h2 className="col p-2 text-center"> Select a calculator</h2>
+        <h2 className="col p-2 text-center"> Select a calculator:</h2>
         <Link to="/" 
         onClick={() => handleNavbarClick("primaryIncome")}
         className="col p-2 m-2"
